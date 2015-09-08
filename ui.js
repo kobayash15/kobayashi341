c2ff752fa3a6ebf48a19e639b3b794a429a99e81
Ken Kobayashi <kobayash15@up.edu>
2015-09-08 10:43:20 -0700

    New version adds 2 new files ui.js and utility.js. Additionally the thenew files create an about button that will show text lipsum when about button is pressed.

---
+++ ui.js
@@ -0,0 +1,33 @@
+/**                                                                                                                                
+ * ui.js                                                                                                                           
+ *                                                                                                                                 
+ * Defines functionality for instrumenting the user-interface.                                                                     
+ *                                                                                                                                 
+ */
+
+var toggle = function() {
+	
+	var el = document.getElementById('about');
+	addClass(el,'show');
+    console.log('About!');
+
+};
+
+var initialize = function() {
+
+    console.log('Initialize!');
+
+    // Grab the 'About' button element, identified by the                                                                          
+    // 'about-btn' id.                                                                                                             
+    var button = document.getElementById('about-btn');
+
+    // From this point forward, when the button is clicked, the                                                                   
+    // toggle function shall be invoked.                                                                                           
+    button.onclick = toggle;
+
+};
+
+// When this file is included at the bottom of the page,                                                                           
+// the js is loaded after the DOM is loaded.  It is a                                                                              
+// good time to initialize the UI elements in the page.                                                                            
+initialize();
